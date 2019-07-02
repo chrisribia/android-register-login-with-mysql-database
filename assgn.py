06ce14deb5d245eb333a0d47ca3093080cbba104
chrisribia <31062468+chrisribia@users.noreply.github.com>
2019-07-02 13:52:10 +0300

    Add files

---
+++ assgn.py
@@ -0,0 +1,14 @@
+age = input("Please enter your age ?")
+income = input("Please enter your income ?")
+
+if 20 < int(age) < 29:
+    tax = int(income)*0.1
+     print(tax)
+elif 29 int(age) <39:
+    tax = int(income) * 0.2
+    print(tax)
+else:
+    tax = int(income) * 0.3
+    print(tax)
+    
+  
